5b695759cdb8353929bcc2cda6b217238f758f94
youngwind <540506594@qq.com>
2016-08-05 17:30:30 +0800

    完成select组件,但是发现flex存在兼容性问题

--- example/example.js
+++ example/example.js
@@ -8,12 +8,55 @@ var VUI = require('../index.js');
 Vue.component('v-number', VUI.number);
 Vue.component('v-checkbox', VUI.checkbox);
 Vue.component('v-rate', VUI.rate);
-Vue.component('v-shade',VUI.shade);
-Vue.component('v-select',VUI.select);
+Vue.component('v-shade', VUI.shade);
+Vue.component('v-select', VUI.select);
 
 new Vue({
     el: '#app',
     data: {
-        number: 1
+        number: 1,
+        selectOptions: [
+            {
+                id: 1,
+                name: "测试数据1"
+            },
+            {
+                id: 2,
+                name: "测试数据测试2"
+            },
+            {
+                id: 3,
+                name: "测试数据测试长度3"
+            },
+            {
+                id: 4,
+                name: "测试数据测试长度3"
+            },
+            {
+                id: 5,
+                name: "测试数据测试长度3"
+            },
+            {
+                id: 6,
+                name: "测试数据测试长度3"
+            },
+            {
+                id: 7,
+                name: "测试数据测试长度3"
+            },
+            {
+                id: 8,
+                name: "测试数据测试长度3"
+            },
+            {
+                id: 9,
+                name: "测试数据测试长度3"
+            },
+            {
+                id: 10,
+                name: "测试数据测试长度3"
+            }
+        ],
+        selectedId: 4
     }
 });
--- select/index.js
+++ select/index.js
@@ -4,33 +4,82 @@
  */
 
 var Vue = require('vue');
-
+var find = require(('lodash.find'));
 
 var vShade = require('../shade');
 Vue.component('v-shade', vShade);
+
 
 require('./index.scss');
 
 var vSelect = Vue.extend({
     template: require('./index.html'),
     props: {
+
+        // 是否显示下拉框
         display: {
             type: [Boolean],
-            default: false
+            default: true
+        },
+
+        // 下拉选项数组对象
+        options: {
+            type: Array,
+            default: function () {
+                return [
+                    {
+                        id: 1,
+                        name: "测试数据1"
+                    },
+                    {
+                        id: 2,
+                        name: "测试数据测试2"
+                    },
+                    {
+                        id: 3,
+                        name: "测试数据测试长度3"
+                    }
+                ]
+            }
+        },
+
+        // 选中option的id
+        selected: {
+            type: [Number, String],
+            default: 2
         }
+
     },
     methods: {
         open: function () {
             this.display = true;
-            console.log(this.display);
+        },
+        selectOption: function (selectedId) {
+            var that = this;
+            this.selected = selectedId;
+
+            // 此处使用延时是为了让用户感知到自己选中了某项
+            // 更好的处理方法是使用动画,而非延时关闭
+            setTimeout(function () {
+                that.close();
+            }, 100)
+        },
+        close: function () {
+            this.display = false;
         }
     },
     computed: {
         contentClass: function () {
             return {
-                'content': true,
+                'v-select-content': true,
                 'v-show': this.display
             }
+        },
+
+        selectedOption: function () {
+            return find(this.options, {
+                id: this.selected
+            }).name
         }
     }
 });
